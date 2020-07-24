147bbeb4dda35e94785814ca9a713c044229b25d
Brooke Storm <bstorm@wikimedia.org>
2020-07-24 14:50:21 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='paws',
-    version='0.1',
+    version='0.2',
     description='Package / Module importer for importing code from Jupyter Notebook files (.ipynb) inside PAWS',
     url='http://github.com/yuvipanda/ipynb-paws',
     author='Yuvi Panda',
